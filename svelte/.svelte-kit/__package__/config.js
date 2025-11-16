ace3a5001e1ebe4e4386888812d084a0d05cbc0f
Yacine Zendaoui <trikooplays@gmail.com>
2025-11-16 11:05:25 +0100

    chore: sync package.json for react and drop svelte support

--- svelte/.svelte-kit/__package__/config.js
+++
@@ -1,8 +0,0 @@
-let config = {};
-try {
-    config = eval("require")("clicons.config");
-}
-catch {
-    config = {};
-}
-export default config;
--- svelte/src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-let config: any = {};
-try {
-  config = eval("require")("clicons.config");
-} catch {
-  config = {};
-}
-export default config;
